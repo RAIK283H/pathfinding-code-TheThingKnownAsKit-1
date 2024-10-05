91767f58c135c0e06848e3ca3227464482614144
Ella Moody <moodyellam@gmail.com>
2024-10-05 15:16:47 -0500

    finished random path

--- pathing.py
+++ pathing.py
@@ -16,7 +16,39 @@ def get_test_path():
 
 
 def get_random_path():
-    return [1,2]
+    current_graph_index = global_game_data.current_graph_index
+    current_graph = graph_data.graph_data[current_graph_index]
+    target_node = global_game_data.target_node[current_graph_index]
+    exit_node_id = len(current_graph) - 1
+    
+    current_node_id = 0
+    path = [current_node_id]
+
+    found_target = False
+    while (not found_target):
+        adjacency_list = current_graph[current_node_id][1]
+
+        if (target_node in adjacency_list):
+            path.append(target_node)
+            found_target = True
+        else:
+            next = random.choice(adjacency_list)
+            path.append(next)
+            current_node_id = next
+    
+    found_exit = False
+    while (not found_exit):
+        adjacency_list = current_graph[current_node_id][1]
+
+        if (exit_node_id in adjacency_list):
+            path.append(exit_node_id)
+            found_exit = True
+        else:
+            next = random.choice(adjacency_list)
+            path.append(next)
+            current_node_id = next
+
+    return path
 
 
 def get_dfs_path():

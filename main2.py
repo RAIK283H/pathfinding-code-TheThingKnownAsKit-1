3e0246b464c034367d56ff16f70b26596c2026b2
Ella Moody <moodyellam@gmail.com>
2024-11-04 13:30:41 -0600

    kinda works kinda loops infinitely

--- main2.py
+++ main2.py
@@ -3,7 +3,9 @@ import global_game_data
 import permutation
 
 def main():
-  permutation.sjt(4)
+  cycles = permutation.sjt(4)
+  print(cycles)
+
 
 if __name__ == '__main__':
   main()
--- permutation.py
+++ permutation.py
@@ -1,37 +1,58 @@
-permutation = {} # False is left, True is right
-largest_mobile = -1
-mobile_index = -1
+import graph_data
 
 def sjt(length):
+  global largest_mobile, mobile_index
   permutation = {i + 1: False for i in range(length)}
+  hamiltonian_cycles = {} # Where [0] is the permutation and [1] is if it's a Hamiltonian cycle
 
-  while largest_mobile is not -1:
+  while True:
     largest_mobile = -1
     mobile_index = -1 
-    findLargestMobileInteger()
+    findLargestMobileInteger(permutation)
 
-    # Move the largest mobile
-    if permutation[largest_mobile] is True: # Moving right
-      permutation[mobile_index], permutation[mobile_index + 1] = permutation[mobile_index + 1], permutation[mobile_index]
-      mobile_index += 1
-    else: # Moving left
-      permutation[mobile_index + 1], permutation[mobile_index] = permutation[mobile_index], permutation[mobile_index + 1]
-      mobile_index -= 1
+    if largest_mobile == -1:
+      break
+
+    # Move the largest mobile element
+    if permutation[largest_mobile]:  # Moving right
+      swap(permutation, largest_mobile, largest_mobile + 1)
+    else:  # Moving left
+      swap(permutation, largest_mobile, largest_mobile - 1)
     
-    # Swap directions of larger integers
-    for element in enumerate(permutation):
-      if element > largest_mobile:
-        permutation[element] = not permutation[element]
+    # Change directions of larger integers
+    for key in permutation.keys():
+      if key > largest_mobile:
+        permutation[key] = not permutation[key]
+  
+    current_perm = tuple(permutation.keys())
+    hamiltonian_cycles[current_perm] = is_hamiltonian_cycle(permutation)
 
-def findLargestMobileInteger():
-  keys = permutation.keys()
+  return hamiltonian_cycles
 
-  for index, element in enumerate(permutation.keys()):
-    if permutation[element] is False:
-      if (index > 0) and (keys[index] > keys[index + 1]) and (keys[index] > largest_mobile):
-        largest_mobile = element
-        mobile_index = index
-    elif permutation[element] is True:
-      if (index < len(permutation) - 1) and (keys[index] > keys[index + 1]) and (keys[index] > largest_mobile):
-        largest_mobile = element
-        mobile_index = index
+def findLargestMobileInteger(permutation):
+  global largest_mobile, mobile_index
+  keys = list(permutation.keys())
+
+  for index, element in enumerate(keys):
+    if permutation[element] is False:  # Moving left
+      if index > 0 and keys[index] > keys[index - 1]:
+        if keys[index] > largest_mobile:
+          largest_mobile = keys[index]
+          mobile_index = index
+    elif permutation[element] is True:  # Moving right
+      if index < len(keys) - 1 and keys[index] > keys[index + 1]:
+        if keys[index] > largest_mobile:
+          largest_mobile = keys[index]
+          mobile_index = index
+
+def swap(permutation, val1, val2):
+  permutation[val1], permutation[val2] = permutation[val2], permutation[val1]
+
+def is_hamiltonian_cycle(permutation):
+  n = len(permutation)
+  if permutation[1] != 1 or permutation[n] != n:  # Start should be 1 and end should be n
+    return False
+  for i in range(1, n):
+    if permutation[i] + 1 != permutation[i + 1] and permutation[i] - 1 != permutation[i + 1]:
+     return False
+  return True

484100a1a540032eb8547dee1b996f2c467438a5
Ella Moody <moodyellam@gmail.com>
2024-12-03 14:18:04 -0600

    no more errors, algorithm not working though

--- f_w.py
+++ f_w.py
@@ -3,7 +3,7 @@ import math
 def floyd_warshall_start(current_graph, target_id):
   # Set up the adjacency matrix
   n = len(current_graph)
-  adj_matrix = [[None] * n for _ in range(n)]
+  adj_matrix = [[math.inf] * n for _ in range(n)]
 
   for i in range(n):
     adj_matrix[i][i] = 0
@@ -14,30 +14,31 @@ def floyd_warshall_start(current_graph, target_id):
       distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
       adj_matrix[i][neighbor] = distance
   
-  return build_path(floyd_warshall(adj_matrix), 0, target_id, current_graph[len(current_graph) - 1])
+  return build_path(floyd_warshall(adj_matrix), 0, target_id, len(current_graph) - 1)
 
 def floyd_warshall(adj_matrix):
   parent_matrix = [[None for i in range(len(adj_matrix))] for j in range(len(adj_matrix))]
 
-  for k in range(len(adj_matrix) - 1):
-    for i in range(len(adj_matrix) - 1):
-      for j in range(len(adj_matrix) - 1):
-        if adj_matrix[i][k] is not None and adj_matrix[k][j] is not None:
-          new_distance = adj_matrix[i][k] + adj_matrix[k][j]
-          if adj_matrix[i][j] is None or new_distance < adj_matrix[i][j]:
-            adj_matrix[i][j] = new_distance
-            parent_matrix[i][j] = parent_matrix[k][j]
+  for k in range(len(adj_matrix)):
+    for i in range(len(adj_matrix)):
+      for j in range(len(adj_matrix)):
+          if adj_matrix[i][k] + adj_matrix[k][j] < adj_matrix[i][j]:
+            adj_matrix[i][j] = adj_matrix[i][k] + adj_matrix[k][j]
+            parent_matrix[i][j] = k
 
   return parent_matrix
 
 def build_path(parent_matrix, start, target, end):
   def build_subpath(parent_matrix, start, end):
+    print (start)
+    print(end)
     path = []
-    current = end
-    while current != start:
+    current = parent_matrix[start][end]
+    while current is not None:
       path.insert(0, current)
       current = parent_matrix[start][current]
     path.insert(0, start)
+    path.append(end)
     return path
 
   path_to_target = build_subpath(parent_matrix, start, target)
